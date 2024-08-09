a09cece7a65b03fede5067e29b0102d997b26235
adamMcneil <adamwmcneil@gmail.com>
2024-08-09 15:39:13 -0500

    clean config

--- init.lua
+++ init.lua
@@ -1,26 +1,3 @@
+require("vim-options")
 require("config.lazy")
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
-vim.g.mapleader = " "
-
-local function map(mode, lhs, rhs, opts)
-    local options = { noremap = true, silent = true }
-    if opts then
-        if opts.desc then
-            opts.desc = "keymaps.lua: " .. opts.desc
-        end
-        options = vim.tbl_extend('force', options, opts)
-    end
-    vim.keymap.set(mode, lhs, rhs, options)
-end
-
-map ("n", "<leader>t", ":NvimTreeToggle<Enter>", {desc = "toggle file tree"}) 
-map ("n", "<leader>f", ":NvimTreeFocus<Enter>", {desc = "focus file tree"}) 
-
-local builtin = require("telescope.builtin")
-map ("n", "<leader>n", builtin.find_files, {desc = "find file"}) 
-
+require("lazy").setup("plugins")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,8 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function() 
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>n", builtin.find_files, {desc = "find file"}) 
+  end
 }
