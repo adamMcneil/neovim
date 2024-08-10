391098d3484f0892384651532d3282af41b63f99
adamMcneil <adamwmcneil@gmail.com>
2024-08-09 20:30:46 -0500

    lazy mapping

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 require("vim-options")
 require("config.lazy")
+
+vim.keymap.set("n", "<leader>l", ":Lazy<CR>", {desc = "open lazy window"}) 
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+
 require("lazy").setup({
   spec = {
     { import = "plugins" },
