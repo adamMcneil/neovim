99ee5da5691a5f7cd8ce54013f25dc83dc93a07b
adamMcneil <adamwmcneil@gmail.com>
2024-08-09 20:17:49 -0500

    added tokyo

---
+++ lua/plugins/tree-sitter.lua
@@ -0,0 +1,20 @@
+local M = {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+}
+
+M.config = function()
+  require("nvim-treesitter").setup()
+
+  local configs = require("nvim-treesitter.configs")
+
+  configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      highlight = { enable = true },
+      indent = { enable = true },  
+  })
+end
+
+return { M }
