da9d97bd77cb5e9fefad1503ecea8d762cc77945
adamMcneil <adamwmcneil@gmail.com>
2024-08-10 21:34:50 -0500

    added auto complete

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -11,7 +11,8 @@ M.config = function()
   local configs = require("nvim-treesitter.configs")
 
   configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
   })
