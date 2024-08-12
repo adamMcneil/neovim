f6ee2dcabd7cc68b969dc826e8d8b620b2a2205f
adamMcneil <adamwmcneil@gmail.com>
2024-08-12 16:36:00 -0500

    added auto lsp install but does not work probably

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,21 +8,34 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "rust_analyzer", "jsonls" }
-      })
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+      -- require("mason-lspconfig").setup({
+      --   ensure_installed = { "lua_ls", "rust_analyzer", "jsonls", "svelte" }
+      -- })
+      require("mason-lspconfig").setup_handlers {
+        function(server_name)
+          require("lspconfig")[server_name].setup {}
+        end,
+        -- Next, you can provide a dedicated handler for specific servers.
+        -- For example, a handler override for the `rust_analyzer`:
+        -- ["rust_analyzer"] = function()
+        --   require("rust-tools").setup {}
+        -- end
+      }
     end
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.jsonls.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {desc = "hover"})
-      vim.keymap.set('n', '<leader>g', vim.lsp.buf.definition, {desc = "go to definition"})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {desc = "code action"})
+      -- local lspconfig = require("lspconfig")
+      -- lspconfig.lua_ls.setup({})
+      -- lspconfig.rust_analyzer.setup({})
+      -- lspconfig.jsonls.setup({})
+      -- lspconfig.svelte.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "hover" })
+      vim.keymap.set('n', '<leader>g', vim.lsp.buf.definition, { desc = "go to definition" })
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "code action" })
     end
   },
 }
